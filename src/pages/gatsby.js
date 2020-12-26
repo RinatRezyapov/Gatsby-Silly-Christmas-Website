accea5bca933da90ecdeaf72bc56adf48e992f74
Rinat Rezyapov <rinat.rezyapov@gmail.com>
2020-12-27 00:05:50 +0500

    Add quotes

--- src/pages/gatsby.js
+++ src/pages/gatsby.js
@@ -18,10 +18,10 @@ export default function Gatsby() {
     <Column>
       <h1>
         Gatsby sites are fast by design
-        </h1>
+      </h1>
       <p>
-        Gatsby loads only critical 1 parts of the page, so your site loads as fast as possible. Once loaded, Gatsby prefetches 2 resources for other pages so that clicking on the site feels incredibly fast. Gatsby lets teams focus on creating content-driven websites instead of worrying about performance.
-        </p>
+        <i>"Gatsby loads only critical 1 parts of the page, so your site loads as fast as possible. Once loaded, Gatsby prefetches 2 resources for other pages so that clicking on the site feels incredibly fast. Gatsby lets teams focus on creating content-driven websites instead of worrying about performance."</i> - gatsbyjs
+      </p>
     </Column>
   </Layout>
 }
