f1a9cb8d4e408e56e2e85f101fcd19e1b73e4121
Rinat Rezyapov <rinat.rezyapov@gmail.com>
2020-12-31 22:57:48 +0500

    Custom favicon is added

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,18 @@ module.exports = {
   /* Your site config here */
   plugins: [
     `gatsby-plugin-sass`, 
-    `gatsby-plugin-styled-components`
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Gatsby Silly Christmas Site Challenge by Rinat Rezyapov`,
+        short_name: `Gatsby Silly Site Challenge`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: '#ffffff',
+        display: `minimal-ui`,
+        icon: `content/assets/icon.png`
+      }
+    },
   ],
 }
